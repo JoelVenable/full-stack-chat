3271f6f06ac2b88c694aa87bb8504949508c2d22
Joel Venable <joelv@auctionedge.com>
2019-12-23 02:45:52 +0000

    Updated apollo config

--- server/src/handler.ts
+++ server/src/handler.ts
@@ -6,10 +6,21 @@ import typeDefs from "./schema/Query.graphql";
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: ({ event, context}) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context,
+  })
 });
 
-module.exports.graphqlHandler = server.createHandler();
+module.exports.graphqlHandler = server.createHandler({
+  cors: {
+    origin: "*",
+    credentials: true
+  }
+});
 
 // Use this code if you don't use the http event with the LAMBDA-PROXY integration
 // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
